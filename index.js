c0fe6eda5d6d4c25781e12543e3145e1af5a30d2
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-15 00:12:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,72 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(employeeArray){
+    return Object.create({
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+
+    })
+}
+
+function createEmployeeRecords(employeeArrays){
+    return employeeArrays.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(dateTimeStamp){
+    let dateTimeStampSplit = dateTimeStamp.split(' ')
+    let newTimeStamp = Object.create({
+        type: "TimeIn",
+        hour: parseInt(dateTimeStampSplit[1]),
+        date: dateTimeStampSplit[0]
+    })
+
+    this.timeInEvents.push(newTimeStamp)
+    return this
+}
+
+function createTimeOutEvent(dateTimeStamp){
+    let dateTimeStampSplit = dateTimeStamp.split(' ')
+    let newTimeStamp = Object.create({
+        type: "TimeOut",
+        hour: parseInt(dateTimeStampSplit[1]),
+        date: dateTimeStampSplit[0]
+    })
+
+    this.timeOutEvents.push(newTimeStamp)
+    return this
+}
+
+function hoursWorkedOnDate(dateWorked){
+    let timeIn = this.timeInEvents.find( ({date}) => date === dateWorked );
+    let timeOut = this.timeOutEvents.find( ({date}) => date === dateWorked)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(dateWorked){
+    let hoursWorked = hoursWorkedOnDate.bind(this)
+    return hoursWorked(dateWorked) * this.payPerHour
+}
+
+function findEmployeeByFirstName(srcArray, name){
+    return srcArray.find(employee => employee.firstName === name)
+}
+
+function calculatePayroll(arrayOfEmployeeRecords){
+
+    let total = 0
+  //  for (let i = 0; i < arrayOfEmployeeRecords.length; i++){
+  //      total += allWagesFor.call(arrayOfEmployeeRecords[i])
+  //  }
+arrayOfEmployeeRecords.forEach(employee =>{
+  total += allWagesFor.call(employee);
+})
+  
+    return total
+
+}
